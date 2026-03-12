ca3f2d5c521043330fce2ce1cbb4b51fa109d6d1
Chadwick Turner <chadwickturner@CT-Mac-Batman.local>
2026-03-12 12:39:55 -0700

    Fix: skip ESLint on build, add OG image 1200x630

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     siteName: 'StokeGames',
-    images: [{ url: '/og-image.jpg', width: 1200, height: 630 }],
+    images: [{ url: '/og-image.png', width: 1200, height: 630 }],
   },
   twitter: {
     card: 'summary_large_image',
